483a1c018b66a0dba650968ba37cf215e1f92be7
Pranay <pranayvjadhao197@gmail.com>
2026-04-09 12:03:00 +0530

    Initial commit

---
+++ Student.py
@@ -0,0 +1,16 @@
+class Student:
+    def __init__(self, name, Roll_number,Department):
+        self.name = name
+        self.Roll_number = Roll_number
+        self.Department = Department
+
+    def display(self):
+        print("Name :",self.name)
+        print("Roll Number :",self.Roll_number)
+        print("Department :",self.Department)
+
+s1 = Student("Pranay", 25,"Entc")
+s2 = Student("Raj", 26,"ENTC")
+s1.display()
+s2.display()
+        
